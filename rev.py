3dc903b8c532b709b174771a50b7d40088df358f
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-05-05 07:14:34 +0530

    python revesion

---
+++ rev.py
@@ -0,0 +1,34 @@
+import random 
+import sys
+import os 
+
+
+#simply printing the message
+print("hello world");
+# writting single line comment
+'''
+multiline comments 
+'''
+# in python u can store any type of variables 
+name="gokuljs"
+print(name);
+value1=10
+value2=1.5
+print(value1);
+print(value2);
+
+# numbers , lists ,tuples, dictionary ,Strings 
+# there are seven diffrent arithematic operaters 
+# +,  -,  *,  /, % ,**(exponential calculations), // floor division
+#Floor division is a normal division operation except that it returns the largest possible integer. This integer is either less than or equal to the normal division result. Floor function is mathematically denoted by this ⌊ ⌋ symbol.
+print("=======================================================================")
+print("5+2",5+2)
+print("5-2",5-2)
+print("5*2",5*2)
+print("5/2",5/2)
+print("5%2",5%2)
+print("5//2",5//2)
+print("5**2",5**2)
+
+
+
