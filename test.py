7b52ddd26ce248580e5e76b6a89de972d2ea31e2
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-05-05 11:26:44 +0530

    random

---
+++ test.py
@@ -0,0 +1,11 @@
+import random 
+
+x=random.randrange(1,100);
+print(x)
+
+for i in range (100):
+    x=random.randrange(1,100);
+    print(x)
+
+# if ur using the random module and then never name ur file random.py
+
