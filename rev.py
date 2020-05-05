937d6e56bfa0cb9478f37698a2347cc7a7207f1b
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-05-05 09:05:54 +0530

    tuples

--- rev.py
+++ rev.py
@@ -71,4 +71,78 @@ print(todolist)
 # combining to lists 
 complete_list=[grocery_list,todolist]
 print(complete_list) ## also an example for list inside an list 
+## now there are twolists inside a list 
+## now i want to use second item in the second list 
+print(complete_list[1][0])
+print(complete_list[1][2])
+print(complete_list[0][3])
+# appending items into the list 
+grocery_list.append('oninons') ## normally this inserts value at the end of the list 
+print(grocery_list)
+## inserting value at very particular index in the list
+##listname(indexvalue,"position you want to insert")
+grocery_list.insert(1,"pickle")
+print(grocery_list)
+# removing item in the list then .remove
+grocery_list.remove("pickle")
+print(grocery_list)
+# sorting item in the list 
+grocery_list.sort();
+print(grocery_list)
+# reverse sorting the list 
+grocery_list.reverse();
+print(grocery_list)
+# deleting an item
+# del grocery_list
+# print(grocery_list)
+# deleteing specific item in list 
+del grocery_list[4]
+print(grocery_list)
+todolist2=todolist+grocery_list
+print(todolist2)
+# getting lenght of the list 
+print(len(todolist2))
+# len id the built in function to get the string 
+# getting maximum element in the list 
+print(max(todolist2))
+'''
+The function is applied to each item on the iterable. If max() is called with an iterable, it returns the largest item in it. If the iterable is empty then the default value is returned, otherwise, a ValueError exception is raised. If max() is called with multiple arguments, it returns the largest one.Jan 7, 2020
+
+'''
+## getting minimum element in the list 
+print(min(todolist2))
+
+
+print("=======================================================================")
+## getting started with tuples 
+
+## tuples are simliar to list 
+## but tuples cannot be changed once inserted 
+## they use parthensis instead of square brackets 
+# tuples can be converted to lists and viceversa
+
+'''
+Tuples are useful for representing what other languages often call records — 
+some related information that belongs together, like your student record. ... So like 
+strings, tuples are immutable. Once Python has created a tuple in memory, it cannot be 
+changed.
+
+'''
+tuple2=(1,2,3,4,5,6,7,9)
+print(tuple2)
+# converting tuple into list 
+
+
+list1=list(tuple2)
+print(list1)
+# converting list to tuple 
+tuple1=tuple(list1)
+print(tuple1)
+print(len(tuple1))
+print(min(tuple1))
+print(max(tuple1))
+print(tuple1[0])
+print(tuple1[1:4])
+
+
 
