924fc63c94f75b8483f208c2d694c59ee9b24ba6
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-05-08 06:27:45 +0530

    math.py

--- mathstrings.py
+++ mathstrings.py
@@ -5,4 +5,15 @@ print(math.floor(4.4)) # floor is going to round down the file
 print(math.fabs(-4.4)) # fabs is going to knock of the aboslute value 
 print(math.factorial(6)) # going to give factorial of a number 
 print(math.fmod(4,2)) # is going to five back reminder of the number 
-print(math.trunc(4.2))
+print(math.trunc(4.2)) # takes in floatting point number and gives u back integer
+print(math.pow(2,2)) # takes two values and power of anumber 
+#special values 
+print(math.e) # value of an exponent 
+print(math.pi)# value of an pi
+print(math.exp(4))
+print(math.log(4))# natural logrithum 
+print(math.log(1000,10))# log value to the base 10
+# can find values for sin,cos,tan,asin.cosin,atan,atan2,asinh,acosh
+# converting degree to radians viceversa
+print(math.degrees(1.5708))
+print(math.radians(90))
