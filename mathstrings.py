ea891962c56b0945578fafb640b4ef53adae731d
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-05-08 08:30:26 +0530

    string2.py

--- mathstrings.py
+++ mathstrings.py
@@ -1,6 +1,6 @@
 import math
 
-print("celi(4.4)",math.ceil(4.4)) # ceiling id going to round up the value 
+print(math.ceil(4.4)) # ceiling id going to round up the value 
 print(math.floor(4.4)) # floor is going to round down the file 
 print(math.fabs(-4.4)) # fabs is going to knock of the aboslute value 
 print(math.factorial(6)) # going to give factorial of a number 
@@ -15,5 +15,5 @@ print(math.log(4))# natural logrithum
 print(math.log(1000,10))# log value to the base 10
 # can find values for sin,cos,tan,asin.cosin,atan,atan2,asinh,acosh
 # converting degree to radians viceversa
-print(math.degrees(1.5708))
+print(math.degrees(1.5708)) 
 print(math.radians(90))
---
+++ string2.py
@@ -0,0 +1,28 @@
+randomstring="     i am going to master python in one day      "
+print(randomstring)
+# going to remove white spaces in left side of the string 
+print(randomstring.lstrip())
+print(randomstring.rstrip())
+print(randomstring.strip())
+# cpatilizining the first letter in the string 
+randomstring=randomstring.strip()
+print(randomstring.capitalize())
+print(randomstring.upper())
+print(randomstring.lower())
+# converting list into a string 
+alist=["gokul","is","good","at","gaming"]
+print(" ".join(alist))
+# converting a list to string
+alist2=randomstring.split()
+print(alist2)
+for i in alist2:
+    print(i)
+# going to find hw many times a string occurs in a string 
+print("going to find hw many times a string occurs in a string ")
+print(randomstring.count("python"))
+# going to find a particular string inside a string 
+print(randomstring.find("python"))
+
+
+
+
