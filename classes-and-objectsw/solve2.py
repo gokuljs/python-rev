a2381f001ac685404bbb8dd4ca9c3519f8653905
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-07-08 10:48:32 +0530

    classes and objects

--- classes-and-objectsw/solve2.py
+++ classes-and-objectsw/solve2.py
@@ -1,5 +1,24 @@
 # getting started with getters and setters
-class Square:
+class Rectangle:
     def __init__(self,height=0,width=0):
         self.height=height
-        self.weight=weight
+        self.width=width
+
+    def run(self):
+        area=self.height*self.width
+        return area
+
+def main():
+    height=int(input("enter the the length of the recatngle "))
+    width=int(input("enter the width of the rectangle "))
+    area1=Rectangle(height,width)
+    rectanglearea=area1.run()
+    print(rectanglearea)
+    
+
+
+
+
+
+
+main()
