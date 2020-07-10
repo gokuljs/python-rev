ad24e4d202b9217e742a65cc0665914a1a7f7a21
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-07-10 09:18:10 +0530

    brut-force method

---
+++ testing.py
@@ -0,0 +1,9 @@
+
+def show_excitement():
+    # Your code
+    message="I am super excited for this course!"
+    return message
+
+for i in range (5):
+    mem=show_excitement()
+    print(mem,end=" ")
