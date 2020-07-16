14b716567579792505ebc445ca06b8141d0bc837
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-07-16 17:04:56 +0530

    recursion

--- geeks_for_geeks/recursion/tail_recursion.py
+++ geeks_for_geeks/recursion/tail_recursion.py
@@ -2,3 +2,11 @@
 #  tail recursions exceuted fast when there input size is large
 #  main reason for the tailrecursion is fast because the caller dosent want to save the state
 # tail recursion is one of the reason that quick sort is faster than merge sort 
+#  for example finding a tail recursive problem using tail recursive method
+def fact(n,k):
+    if n==0 or n==1:
+        print(k)
+    fact(n-1,n*k)
+n=int(input("enter your number for tail recursion"))
+fact(n,1)
+
