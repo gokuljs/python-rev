990af9c2a42431ed6f9966fd23a5dd40028bd19d
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-09-02 06:32:28 +0530

    testing

--- testing.py
+++ testing.py
@@ -1,9 +1,12 @@
-
-def show_excitement():
-    # Your code
-    message="I am super excited for this course!"
-    return message
-
-for i in range (5):
-    mem=show_excitement()
-    print(mem,end=" ")
+# birtday chocklate 
+n=int(input("enetr the size of the chocklate"))
+s=[int(ele) for ele in input().split()]
+d,m=input().split()
+d=int(d)
+m=int(m)
+count=0
+# print(d)
+for i in range(0,n-m+1):
+    if sum(s[i:i+m])==d:
+        count=count+1
+print(count)
