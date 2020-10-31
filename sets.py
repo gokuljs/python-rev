bc4dd6fe3b1de028b3124d79c43ba44ff76d4bc5
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-10-31 10:48:03 +0530

    sets in python

--- sets.py
+++ sets.py
@@ -5,7 +5,15 @@ f.add(9)# adding new element into the set
 list2=[2,2,2,5,534,232]
 f.update(list2)
 # removing an element in the set 
-print(f)
-f.remove(2)
-f.discard(32)
+# print(f)
+f.remove(2)# will show an error if elelment is present 
+f.discard(32)# will not show an error if element is not present 
+# print(f)
+
+k=[1,2,3,4]
+k=set(k)
+l=[2,3,5,6,7,4]
+l=set(l)
+# f=k.intersection(l)
+f=k.difference(l)
 print(f)
